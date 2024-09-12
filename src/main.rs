8b7f81c57df8608ecf29467b2370201e54950939
Tanuj Ravi Rao <tanujrao99@gmail.com>
2024-09-12 15:45:10 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #![no_std]
 #![no_main]
 
-fn main() {
-}
-
 use core::panic::PanicInfo;
 
-/// Function called to handle panics
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// Function called to handle panics
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
